b95e61560374b2171b4dfc392f050b4a8d60753c
Kenny Nguyen <knguyen111601@gmail.com>
2021-11-13 14:46:56 -0800

    Made header with the right name and stuff just needs style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import {Link} from "react-router-dom"
+
 const Header = (props) =>{
-    return <h1>Header</h1>
+    return <nav className="nav">
+        <Link to="/">
+            <div>Bookmark'd</div>
+        </Link>
+    </nav>
 }
 
 export default Header
